78d0f9cf17cef2180c5ed6ef786f54e4b84b6545
Yoshito21 <124442793+Yoshito21@users.noreply.github.com>
2023-04-16 20:55:27 +0900

    検索機能中のミス

---
+++ app/models/suppression.rb
@@ -0,0 +1,12 @@
+class Suppression < ActiveHash::Base
+    self.data = [
+      { id: 1, name: 'Fat suppression' },
+      { id: 2, name: 'SPAIR' },
+      { id: 3, name: 'Fluid suppression' },
+      { id: 4, name: 'FLASH' }
+    ]
+  
+    include ActiveHash::Associations
+    has_many :conditions
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cases#index'
-  resources :cases, only: [:index]
+  resources :cases do
+    collection do
+      get 'search'
+    end
+  end
+  #resources :categories, only: [:index, :new, :create]
+  #get '/category/:id', to: 'categories#search'
 end
