0ac1e4a851a310d44ca9c89d00de92f25c64b6ca
Yoshito21 <124442793+Yoshito21@users.noreply.github.com>
2023-06-21 21:57:27 +0900

    デプロイ作業

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack');
-
-environment.config.merge({
-  resolve: {
-    alias: {
-      bootstrap: 'bootstrap/dist',
-    },
-  },
-});
-  
 module.exports = environment
