ab6dd4b069bb3a97918d4b629ba7a159693d313f
Yoshito21 <124442793+Yoshito21@users.noreply.github.com>
2023-06-26 18:34:00 +0900

    調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+admin_occupation_1 = Occupation.create(post_code:"000-0000", prefecture1_id: 40, phone_number:"0000000000", name: "yh_hospital")
+default_occupation = Occupation.create(post_code:"000-0000", prefecture1_id: 13, phone_number:"0123456789", name: "未登録")
+test_occupation_1 = Occupation.create(post_code:"111-1111", prefecture1_id: 1, phone_number:"1111111111", name: "a_hospital")
+test_occupation_2 = Occupation.create(post_code:"222-2222", prefecture1_id: 1, phone_number:"2222222222", name: "b_hospital")
+
+admin_user_1 = User.create(nickname: "和井 英知", email: "yh@gmail.com", password: "yh4410" , prefecture2_id: 40, occupation_id: 1)
+test_user_1 = User.create(nickname: "a", email: "a@a.a", password: "aa1111" , prefecture2_id: 1, occupation_id: 3)
+test_user_2 = User.create(nickname: "aa", email: "aa@a.a", password: "aaaa11" , prefecture2_id: 1, occupation_id: 3)
+test_user_3 = User.create(nickname: "b", email: "b@b.b", password: "bb1111" , prefecture2_id: 1, occupation_id: 4)
+
+imaging_1 = Imaging.new(
+site_id: 1,
+purpose: "急性脳梗塞",
+identification: "DWIで高信号、ADCで低信号",
+symptoms: "めまい・吐き気・呂律不良・四肢の脱力",
+treatment: "・血栓溶解療法（t-PA）・血栓回収療法",
+)
+imaging_1.save!
 
 imaging_2 = Imaging.new(
 site_id: 1,
