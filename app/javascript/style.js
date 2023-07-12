44d6c6e3c3333993919ba650848c4966750f73c1
Yoshito21 <124442793+Yoshito21@users.noreply.github.com>
2023-07-12 12:04:15 +0900

    スタイリング

--- app/javascript/style.js
+++ app/javascript/style.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function () {
     const selectOptions = document.querySelectorAll('select option');
     selectOptions.forEach(option => {
     option.addEventListener('mouseover', function() {
-        this.style.backgroundColor = '#2a2a2a';
+        this.style.backgroundColor = '#72bce7';
     });
     option.addEventListener('mouseout', function() {
         this.style.backgroundColor = '';
